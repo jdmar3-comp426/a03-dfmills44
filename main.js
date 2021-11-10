27b654e106efe002f7ab8783ffcd4ed5a3e1f7ac
dfmills44 <dfmills@live.unc.edu>
2021-11-10 14:09:53 -0500

    Medium 1 Draft 1

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
-import {sumToString} from "./src/mild/mild_1.js";
-import {getIncreasingArray} from "./src/mild/mild_1.js";
-import {maxAndMin} from "./src/mild/mild_1.js";
-import {countArray} from "./src/mild/mild_1.js";
-import {identifyVariable, removeKey} from "./src/mild/mild_2.js";
-import {identifyArray} from "./src/mild/mild_2.js";
+import {getSum} from './src/medium/medium_1.js'
+import {getMedian} from './src/medium/medium_1.js'
+import {getStatistics} from './src/medium/medium_1.js'
 
-console.log(sumToString(75,63));
-console.log(getIncreasingArray(1,50));
-console.log(maxAndMin([2,9,1,-3,5]));
-console.log(countArray([1, 5, 1, 2, 2, 'night', 'day', 'night', true, true, false, [1,2], [1,2], [3,4]]));
-console.log(identifyVariable(4));
-console.log(identifyArray(['some', 3, [3, 4], false]));
-var obj = {
-    name: 'Mr. Boss',
-    title: 'boss',
-    age: 33,
-    password: 'pass123'
-};
-removeKey(obj, 'password')
-console.log(obj);
+console.log(getSum([1, 3, 5, -2, 6]))
+console.log(getMedian([3,7,5,6,2,2,4,2,7,5]));
+console.log(getStatistics([3,2,4,5,5,5,2,6,7]));
